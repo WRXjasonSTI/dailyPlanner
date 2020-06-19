3eeeb9031b93e0aa36102b991eb248f5534dbcbb
JCZ-020 <jasoncruz_rocks@yahoo.com>
2020-06-18 23:57:08 -0400

    clean

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ $(time).text(currentTime);
 // moment object
 var m = moment.parseZone()
 var today = moment.parseZone()
-// arrays for working hours 9-5
+// array to set hours
 var workHours = [m.startOf('day').add(9, 'hours').format("hh:mm a"),m.startOf('day').add(10, 'hours').format("hh:mm a"),m.startOf('day').add(11, 'hours').format("hh:mm a"),m.startOf('day').add(12, 'hours').format("hh:mm a"),m.startOf('day').add(13, 'hours').format("hh:mm a"),m.startOf('day').add(14, 'hours').format("hh:mm a"),m.startOf('day').add(15, 'hours').format("hh:mm a"),m.startOf('day').add(16, 'hours').format("hh:mm a"),m.startOf('day').add(17, 'hours').format("hh:mm a"),m.startOf('day').add(18, 'hours').format("hh:mm a")]
 var workHoursHH = [m.startOf("day").add(9,"hours").format("HH"),m.startOf("day").add(10,"hours").format("HH"),m.startOf("day").add(11,"hours").format("HH"),m.startOf("day").add(12,"hours").format("HH"),m.startOf("day").add(13,"hours").format("HH"),m.startOf("day").add(14,"hours").format("HH"),m.startOf("day").add(15,"hours").format("HH"),m.startOf("day").add(16,"hours").format("HH"),m.startOf("day").add(17,"hours").format("HH"),m.startOf("day").add(18,"hours").format("HH")]
 
@@ -36,11 +36,10 @@ var tasks = {
 
 // initialize check for existing tasks then check for existing entires
 function init() {
-    // Get stored tasks from localStorage
-    // Parsing the JSON string to an object
+    // recalling local storage
     var storedTasks = JSON.parse(localStorage.getItem("tasks"));
         
-    // setting stored items
+    // setting local storage
     if (storedTasks !== null) {
         tasks = storedTasks;
     } else{
@@ -52,7 +51,7 @@ function init() {
 init();
 
 function renderWorkHours() {
-    // first clear out the tasklist
+    // effective initialize with clearing
     taskList.empty()
         // loop through work hours, also creating Bootstrap based sections
         for (i = 0; i < workHours.length-1; i++) {
@@ -94,14 +93,11 @@ function renderWorkHours() {
         saveButton.appendTo(newDiv);
         // append the div to the taskList
         newDiv.appendTo(taskList)
-
-
         }
-
 }
     
     function storeTask() {
-        // Stringify and set "tasks" key in localStorage to TASKS array
+        // formatting
         localStorage.setItem("tasks", JSON.stringify(tasks));
     }   
 
@@ -114,17 +110,9 @@ function renderWorkHours() {
     $(".saveBtn").click(function (e) { 
         e.preventDefault();
         var timeBlock  = $(this).attr("data-value");
-
-        // get the current val of the textarea for that
         var areaVal = $("#txt"+timeBlock).val().trim()
-
-        // first remove the current val in the time block from the stored array
-        // then, store the new task value in the task array.
         tasks.timeBlks.splice((timeBlock),1,(areaVal))
-
-        // store the task list
         storeTask();
-        // refresh the dom so the clicking the save button event can happen multiple times
         document.reload();
     });
 
